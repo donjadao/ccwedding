7ece692ef2a7acba926a0d7c349e32d843fc1d5a
donjadao <ding.don908@gmail.com>
2026-08-22 13:15:14 -0700

    Update x position of text element in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -149,7 +149,7 @@ function Logomark({ size = 200 }: { size?: number }) {
           D
         </text>
         <text
-          x="95"
+          x="100"
           y="116"
           fontFamily="'Cinzel', serif"
           fontSize="44"
